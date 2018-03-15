eec7647e7ea53c54c4ace60c06cf2aefbdb08b19
scanf <scanf@scanfsec.com>
2018-03-15 14:00:27 +0800

    test

--- 204lab/204lab_cloud_compute.sh
+++ 204lab/204lab_cloud_compute.sh
@@ -32,6 +32,7 @@ yum -y install kvm qemu-kvm qemu-kvm-tools libvirt libguestfs-tools virt-install
 
 #add authorized_keys
 mkdir ~/.ssh/
+chmod 0700 ~/.ssh/
 cat<<_EOF_>>~/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC6egAo9Fws/ULWy/98518SNlOkNnGbf/zwwzxSOJnOBnHKNs1NBK97tiqeyj/8GYgfVA+HDjKK2x6UGLPWZZiTwvmHAilX6Zt6AiPBrgTXIZPZW6hyDonRBi/+3P5LLTvq7QqIpWq6Ndsa8896otCRdW0g+uzD8UmIg7w0M45R/mphE/kh0VK0wqbXohtMhVmPCUcStTq21cLvLvEhR8xa5Zpe9kSAKytEAxgGwdvz0dyI0sYdMLtb8BxyGGFXj9rhNbOJas3gYJWVNRVOlRqbBpK7kxBL+/P6QVl/4AtNEGzp1prnqCHTGgeq6m8mWDh+6aXYO8/DJ6iYoD/ywYBX scanf@scanfsec.com
 _EOF_
